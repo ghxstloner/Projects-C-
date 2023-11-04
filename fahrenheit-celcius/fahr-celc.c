57245bb7c73d9ee8865eb5bee5b7c8e48c13567c
ghxstloner <yoinerdeyhan@gmail.com>
2023-11-04 02:43:50 -0500

    "homework"

--- fahrenheit-celcius/fahr-celc.c
+++ fahrenheit-celcius/fahr-celc.c
@@ -15,3 +15,13 @@ int main() {
         fahr = fahr + step;
     }
 }
+
+/* imprime la tabla Fahrenheit-Celsius
+Otra forma utiliando el Búcle FOR 
+main( )
+{
+int fahr;
+for (fahr=0; fahr<=300; fahr=fahr+20)
+printf('%3d %6.1f\n', fahr, (5.0/9.0)*(fahr-32));
+}
+*/
